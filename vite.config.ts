79a896b39a6f24586c822a69b98f97995815b7ad
vvlvm <temp@example.com>
2026-01-05 08:11:00 +0900

    publicを公開するように修正。viteのbaseを修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src/'),
-		},
-	},
-	server: {
-		open: true,
-	},
+  plugins: [react(), tailwindcss()],
+  base: "/fallout-recipe/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+    },
+  },
+  server: {
+    open: true,
+  },
 })
