32b634dea8a13f00e4349730153492f61663aebe
vvlvm <temp@example.com>
2026-01-11 02:30:17 +0900

    server.host: true

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),],
-  base: "/fallout-recipe/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src/"),
-    },
-  },
-  server: {
-    open: true,
-  },
+	plugins: [react()],
+	base: '/fallout-recipe/',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src/'),
+		},
+	},
+	server: {
+		host: true,
+		open: true,
+	},
 })
