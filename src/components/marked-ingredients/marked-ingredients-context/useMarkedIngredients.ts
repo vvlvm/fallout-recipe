b293f4c169bfaca772f2669cab0753252d511c39
vvlvm <temp@example.com>
2026-01-25 21:22:45 +0900

    使う側で型が分かりづらかったのでreturn型を追加

--- src/components/marked-ingredients/marked-ingredients-context/useMarkedIngredients.ts
+++ src/components/marked-ingredients/marked-ingredients-context/useMarkedIngredients.ts
@@ -1,6 +1,10 @@
 import { MarkedIngredientsContext } from '@/components/marked-ingredients/marked-ingredients-context/MarkedIngredientsContext.tsx'
+import type { MarkedIngredientsValue } from '@/components/marked-ingredients/marked-ingredients-context/MarkedIngredientsValue'
 import { useContext } from 'react'
 
-export function useMarkedIngredients() {
+/**
+ * @returns Set<IngredientName>
+ */
+export function useMarkedIngredients(): MarkedIngredientsValue {
 	return useContext(MarkedIngredientsContext)
 }
