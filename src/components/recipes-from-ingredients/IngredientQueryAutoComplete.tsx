2e15acb6c4c64e63306ff8f1d6303fa07a117016
vvlvm <temp@example.com>
2026-01-25 23:54:23 +0900

    recipes-from-ingredients用のオートコンプリートを作成

---
+++ src/components/recipes-from-ingredients/IngredientQueryAutoComplete.tsx
@@ -0,0 +1,77 @@
+import { INGREDIENT_NAMES } from '@/nuka-mixer-recipe/INGREDIENT_NAMES'
+import Autocomplete, {
+	type AutocompleteChangeReason,
+	type AutocompleteInputChangeReason,
+} from '@mui/material/Autocomplete'
+import TextField from '@mui/material/TextField'
+import type { FilterOptionsState } from '@mui/material/useAutocomplete'
+import { QueryExplain } from './QueryExplain'
+
+const lastTokenRegex = /[^,|、]+$/
+
+// コンポーネントの外に出すことで、レンダリングごとの関数再生成を防ぐ
+function filterOptions(options: string[], state: FilterOptionsState<string>) {
+	const match = state.inputValue.match(lastTokenRegex)
+	const searchTerm = match ? match[0].trim() : ''
+	return options.filter((option) =>
+		option.toLowerCase().includes(searchTerm.toLowerCase()),
+	)
+}
+
+interface Props {
+	inputValue: string
+	setInputValue: React.Dispatch<React.SetStateAction<string>>
+}
+
+export function IngredientQueryAutoComplete(props: Props) {
+	const { inputValue, setInputValue } = props
+
+	function handleInputChange(
+		_: unknown,
+		newInputValue: string,
+		reason: AutocompleteInputChangeReason,
+	) {
+		if (reason !== 'reset') {
+			setInputValue(newInputValue)
+		}
+	}
+
+	function handleChange(
+		_: unknown,
+		newValue: string | null,
+		reason: AutocompleteChangeReason,
+	) {
+		if (reason === 'selectOption' && typeof newValue === 'string') {
+			const match = inputValue.match(lastTokenRegex)
+			let newQuery = ''
+			if (match) {
+				const token = match[0]
+				const leadingSpace = token.match(/^\s*/)?.[0] ?? ''
+				newQuery =
+					inputValue.substring(0, match.index) + leadingSpace + newValue
+			} else {
+				newQuery = inputValue + newValue
+			}
+
+			setInputValue(newQuery)
+		}
+	}
+
+	return (
+		<>
+			<Autocomplete
+				freeSolo
+				options={INGREDIENT_NAMES}
+				inputValue={inputValue}
+				value={inputValue}
+				onInputChange={handleInputChange}
+				onChange={handleChange}
+				filterOptions={filterOptions}
+				renderInput={(params) => (
+					<TextField {...params} placeholder='クアンタム&チェリー' />
+				)}
+			/>
+			<QueryExplain />
+		</>
+	)
+}
---
+++ src/components/recipes-from-ingredients/QueryExplain.tsx
@@ -0,0 +1,114 @@
+import List, { type ListProps } from '@mui/material/List'
+import ListItem, { type ListItemProps } from '@mui/material/ListItem'
+import Paper from '@mui/material/Paper'
+import Stack, { type StackProps } from '@mui/material/Stack'
+import Typography, { type TypographyProps } from '@mui/material/Typography'
+
+const Container = (props: ListProps) => (
+	<List
+		dense
+		{...props}
+		sx={{
+			display: 'grid',
+			gridTemplateColumns: 'auto 1fr',
+			p: 0,
+			m: 0,
+			ml: (theme) => ({ xs: theme.spacing(0.5), sm: theme.spacing(1) }),
+			mt: (theme) => ({ xs: theme.spacing(0.5), sm: theme.spacing(1) }),
+			columnGap: (theme) => ({
+				xs: theme.spacing(1),
+				sm: theme.spacing(3),
+			}),
+			...props.sx,
+		}}
+	/>
+)
+
+const Row = (props: ListItemProps) => (
+	<ListItem
+		{...props}
+		sx={{
+			display: 'grid',
+			gridColumn: 'span 2',
+			gridTemplateColumns: 'subgrid',
+			m: 0,
+			p: 0,
+			pl: (theme) => theme.spacing(0.5),
+			py: (theme) => theme.spacing(0.5),
+			alignItems: 'baseline',
+		}}
+	/>
+)
+
+const Head = (props: TypographyProps) => {
+	return <Typography variant='body2' {...props} />
+}
+
+const WrappableStack = (props: StackProps) => (
+	<Stack direction='row' flexWrap='wrap' rowGap={0.5} {...props} />
+)
+
+const ExampleContainer = (props: StackProps) => (
+	<Stack direction='row' alignItems='end' {...props} />
+)
+
+const Example = (props: TypographyProps) => (
+	<Typography
+		variant='body2'
+		{...props}
+		sx={{
+			whiteSpace: 'pre',
+			border: 'solid 1px',
+			borderColor: (theme) => theme.palette.text.secondary,
+			marginRight: '1px',
+			...props.sx,
+		}}
+	/>
+)
+
+const Note = (props: TypographyProps) => (
+	<Typography
+		variant='caption'
+		{...props}
+		sx={{
+			color: (theme) => theme.palette.text.secondary,
+			whiteSpace: 'nowrap',
+			...props.sx,
+		}}
+	/>
+)
+
+export function QueryExplain() {
+	return (
+		<Paper>
+			<Container>
+				<Row>
+					<Head>orとして扱われる文字</Head>
+					<WrappableStack>
+						<ExampleContainer>
+							<Example
+								sx={{
+									paddingLeft: '2px',
+									paddingRight: '2px',
+								}}
+							>
+								|
+							</Example>
+							<Note>(パイプ)</Note>
+							<Typography sx={{ mr: 1 }}>,</Typography>
+						</ExampleContainer>
+						<ExampleContainer>
+							<Example sx={{ px: '2px' }}>,</Example>
+							<Note>(コンマ)</Note>
+							<Typography sx={{ mr: 1 }}>,</Typography>
+						</ExampleContainer>
+						<ExampleContainer>
+							<Example>、</Example>
+							<Note>(全角読点)</Note>
+						</ExampleContainer>
+					</WrappableStack>
+				</Row>
+			</Container>
+		</Paper>
+	)
+}
